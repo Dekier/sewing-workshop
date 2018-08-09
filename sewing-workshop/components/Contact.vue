0007990403e09faaa8146a5f47dfa1441bfcb1e0
Marcin Dekier <marcin.dekier@appunite.com>
2018-08-09 08:05:29 +0200

    add new contact without responsiveness

---
+++ sewing-workshop/components/Contact.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="Contact__main-container">
+    <div class="Contact__center">
+      <div class="Contact__half-container">
+        <span class="Contact__title">
+          Kontakt
+        </span>
+        <span class="Contact__text">
+          ul. Jaroczyńskiego 41
+        </span>
+
+        <span class="Contact__text">
+          60-692 Poznań-Piątkowo
+        </span>
+
+        <span class="Contact__text">
+          Poniedziałek 12 - 18  
+        </span>
+
+        <span class="Contact__text">
+          Wtorek-Piątek 9 - 17
+        </span>
+
+        <span class="Contact__text">
+          Telefony: 691 860 192
+        </span>
+
+        <span class="Contact__text">
+          E-mail: magdalenadekier@op.pl
+        </span>
+      </div>
+      <div class="Contact__half-container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2431.4108610524404!2d16.90338781606623!3d52.45358617980165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47044341d3556dc5%3A0xd4910db4433e5919!2sPRACOWNIA+KRAWIECKA+Magdaleny+Dekier+Pozna%C5%84!5e0!3m2!1spl!2spl!4v1533744299605" width="500" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
+
+<style lang="scss">
+  @import 'Contact';
+</style>
+
