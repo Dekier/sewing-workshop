7f6bdbd9964ee91fab5e027f00e5c794587c55b3
Dekier <marcin.dekier97@gmail.com>
2019-04-29 17:38:47 +0200

    add logo dezal

--- sewing-workshop/components/Contact.vue
+++ sewing-workshop/components/Contact.vue
@@ -1,8 +1,14 @@
 <template>
-  <div
-  id="contact"
-  class="Contact__main-container">
+  <div id="contact" class="Contact__main-container">
     <div class="Contact__center">
+      <div class="Contact__half-container">
+        <span class="Contact__title">
+          Polecamy
+        </span>
+        <a target="_blank" href="http://dezalroletypoznan.pl">
+          <div class="Contact__logo-dezal" />
+        </a>
+      </div>
       <div class="Contact__half-container">
         <span class="Contact__title">
           Kontakt
@@ -16,7 +22,7 @@
         </span>
 
         <span class="Contact__text">
-          Poniedziałek 12 - 18  
+          Poniedziałek 12 - 18
         </span>
 
         <span class="Contact__text">
@@ -31,10 +37,15 @@
           E-mail: magdalenadekier@op.pl
         </span>
       </div>
-      <div
-      id="map"
-      class="Contact__half-container">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2431.4108610524404!2d16.90338781606623!3d52.45358617980165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47044341d3556dc5%3A0xd4910db4433e5919!2sPRACOWNIA+KRAWIECKA+Magdaleny+Dekier+Pozna%C5%84!5e0!3m2!1spl!2spl!4v1533744299605" width="500" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+      <div id="map" class="Contact__half-container">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2431.4108610524404!2d16.90338781606623!3d52.45358617980165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47044341d3556dc5%3A0xd4910db4433e5919!2sPRACOWNIA+KRAWIECKA+Magdaleny+Dekier+Pozna%C5%84!5e0!3m2!1spl!2spl!4v1533744299605"
+          width="500"
+          height="400"
+          frameborder="0"
+          style="border:0"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   </div>
@@ -42,11 +53,11 @@
 
 <script>
 export default {
-  name: 'Contact'
-}
+  name: "Contact"
+};
 </script>
 
 <style lang="scss">
-  @import 'Contact';
+@import "Contact";
 </style>
 
--- sewing-workshop/plugins/aos.js
+++ sewing-workshop/plugins/aos.js
@@ -3,5 +3,8 @@ import AOS from "aos"
 import "aos/dist/aos.css"
 
 export default ({ app }) => {
-  app.AOS = new AOS.init({ disable: "phone" })
+  app.AOS = new AOS.init({
+    disable: true,
+    once: true,
+  })
 };
