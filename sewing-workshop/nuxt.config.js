a3098461ae44fd9064c23a7eb9aa38ddc1c15f4a
Marcin Dekier <marcin.dekier97@gmail.com>
2026-05-01 21:04:51 +0200

    add ico

--- sewing-workshop/nuxt.config.js
+++ sewing-workshop/nuxt.config.js
@@ -3,14 +3,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Magdalena Dekier - Pracownia Krawiecka',
+    title: 'Usługi Krawieckie Poznań - przeróbki krawieckie',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Pracownia Krawiecka Magdaleny Dekier, szeroki zakres usług krawieckich w Poznaniu na Piątkowie.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Cinzel+Decorative' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Muli:200,400' }
     ]
