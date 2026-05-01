9a059c8f7dfc856d99dc85209eaf50c929e714cd
Marcin Dekier <marcin.dekier97@gmail.com>
2026-05-01 21:04:56 +0200

    update-landing

--- components/AboutCompany.vue
+++ components/AboutCompany.vue
@@ -1,28 +1,28 @@
 <template>
-  <div
-  id="company"
-  class="AboutCompany__main-container">
-    <div
-    class="AboutCompany__text-container">
-       <h3 class="AboutCompany__subtitle">
-        Moja pasja
-      </h3>
+  <div id="company" class="AboutCompany__main-container">
+    <div class="AboutCompany__text-container">
+      <h3 class="AboutCompany__subtitle">Moja pasja</h3>
+      <p
+        class="AboutCompany__article"
+      >Wszelkie usługi krawieckie w Poznaniu to moja pasja i specjalność.Serdecznie zapraszam do Pracowni Krawickiej Magdaleny Dekier, którą prowadzę od 1993r. Przerabiam odzież i dopasowuję do odpowiednich wymiarów sylwetki Pani lub Pana. Zważam, aby klient czuł się w danej rzeczy dobrze, wygodnie i niepowtarzalnie. Do każdego zlecenia - projektu podchodzę indywidualnie. Przeróbki krawieckie wykonuję w sposób profesjonalny, aby rzeczy mogły pięknie wyglądać i jak najdłużej służyć.</p>
       <p class="AboutCompany__article">
-        Wszelkie usługi krawieckie w Poznaniu to moja pasja i specjalność.Serdecznie zapraszam do Pracowni Krawickiej Magdaleny Dekier, którą prowadzę od 1993r. Przerabiam odzież i dopasowuję do odpowiednich wymiarów sylwetki Pani lub Pana. Zważam, aby klient czuł się w danej rzeczy dobrze, wygodnie i niepowtarzalnie. Do każdego zlecenia - projektu podchodzę indywidualnie. Przeróbki krawieckie wykonuję w sposób profesjonalny, aby rzeczy mogły pięknie wyglądać i jak najdłużej służyć. Część pracy wykonuję ręcznie w tradycyjmej technologi krawieckiej lub używam specjalistycznych maszyn dzięki czemu usługa jest wysokiej jakości. W pracowni służę fachową poradą. Cenię sobie punktualność, rzetelność, dokładność.
-        <br>Czas realizacji 1 - 7 dni roboczych. Powiadamiam sms - em gdy usługa jest gotowa do odbioru.<br>
-        Serdecznie zapraszam do siedziby mojej pracowni krawieckiej, która mieści się w Poznaniu na Piątkowie <br>Ul. Jaroczyńskiego 41 vis a vis os. Jagiełły.
+        Część pracy wykonuję ręcznie w tradycyjmej technologi krawieckiej lub używam specjalistycznych maszyn dzięki czemu usługa jest wysokiej jakości. W pracowni służę fachową poradą. Cenię sobie punktualność, rzetelność, dokładność.
+        Czas realizacji 1 - 7 dni roboczych. Powiadamiam sms - em gdy usługa jest gotowa do odbioru.
+      </p>
+      <p class="AboutCompany__article">
+        Serdecznie zapraszam do siedziby mojej pracowni krawieckiej, która mieści się w Poznaniu na Piątkowie
+        <br />Ul. Jaroczyńskiego 41 vis a vis os. Jagiełły.
       </p>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'AboutCompany',
-}
+  name: "AboutCompany"
+};
 </script>
 
 <style lang="scss">
-  @import 'AboutCompany';
+@import "AboutCompany";
 </style>
--- components/Contact.vue
+++ components/Contact.vue
@@ -1,63 +1,87 @@
 <template>
   <div id="contact" class="Contact__main-container">
     <div class="Contact__center">
+      <span class="Contact__title">
+        Kontakt
+      </span>
       <div class="Contact__half-container">
-        <span class="Contact__title">
-          Polecamy
-        </span>
-        <a target="_blank" href="http://dezalroletypoznan.pl">
-          <div class="Contact__logo-dezal" />
-        </a>
+        <div class="Contact__half-container-cell">
+          <div class="Contact__half-container-title">
+            Address
+          </div>
+          <span class="Contact__text">
+            ul. Jaroczyńskiego 41
+          </span>
+          <span class="Contact__text">
+            60-692 Poznań-Piątkowo
+          </span>
+        </div>
+        <div class="Contact__half-container-cell">
+          <div class="Contact__half-container-title">
+            Godziny otwarcia
+          </div>
+          <span class="Contact__text">
+            Poniedziałek: 12:00 - 18:00
+          </span>
+          <span class="Contact__text">
+            Wtorek-Piątek: 9:00 - 17:00
+          </span>
+        </div>
+        <div class="Contact__half-container-cell">
+          <div class="Contact__half-container-title">
+            Skontaktuj się z nami przez:
+          </div>
+          <span class="Contact__text">
+            Telefon: 691 860 192
+          </span>
+          <span class="Contact__text">
+            E-mail: magdalenadekier@op.pl
+          </span>
+        </div>
       </div>
-      <div class="Contact__half-container">
-        <span class="Contact__title">
-          Kontakt
-        </span>
-        <span class="Contact__text">
-          ul. Jaroczyńskiego 41
-        </span>
+    </div>
 
-        <span class="Contact__text">
-          60-692 Poznań-Piątkowo
-        </span>
-
-        <span class="Contact__text">
-          Poniedziałek 12 - 18
-        </span>
-
-        <span class="Contact__text">
-          Wtorek-Piątek 9 - 17
-        </span>
-
-        <span class="Contact__text">
-          Telefony: 691 860 192
-        </span>
-
-        <span class="Contact__text">
-          E-mail: magdalenadekier@op.pl
-        </span>
-      </div>
-      <div id="map" class="Contact__half-container">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2431.4108610524404!2d16.90338781606623!3d52.45358617980165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47044341d3556dc5%3A0xd4910db4433e5919!2sPRACOWNIA+KRAWIECKA+Magdaleny+Dekier+Pozna%C5%84!5e0!3m2!1spl!2spl!4v1533744299605"
-          width="500"
-          height="400"
-          frameborder="0"
-          style="border:0"
-          allowfullscreen
-        ></iframe>
-      </div>
+    <iframe
+      title="Pracownia Krawiecka Magdaleny Dekier"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2431.4108610524404!2d16.90338781606623!3d52.45358617980165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47044341d3556dc5%3A0xd4910db4433e5919!2sPRACOWNIA+KRAWIECKA+Magdaleny+Dekier+Pozna%C5%84!5e0!3m2!1spl!2spl!4v1533744299605"
+      :width="getWindowSize()"
+      height="410"
+      frameborder="0"
+      style="border:0"
+      allowfullscreen
+    ></iframe>
+    <div class="Contact__footer">
+      <span class="Contact__title">
+        Polecamy:
+      </span>
+      <a target="_blank" href="http://dezalroletypoznan.pl" rel="noopener">
+        <div class="Contact__logo-dezal" />
+      </a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Contact"
+  name: "Contact",
+
+  data: () => ({
+    windowWidth: 0
+  }),
+
+  mounted() {
+    this.getWindowSize();
+    window.addEventListener("resize", this.getWindowSize);
+  },
+
+  methods: {
+    getWindowSize() {
+      this.windowWidth = window.innerWidth;
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 @import "Contact";
 </style>
-
