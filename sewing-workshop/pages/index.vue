04f8856ddf88c0017b6adf21674a891da1815587
Marcin Dekier <marcin.dekier97@gmail.com>
2026-05-01 21:04:49 +0200

    without animation but with about company

--- sewing-workshop/pages/index.vue
+++ sewing-workshop/pages/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="Page__main-container">
     <Intro/>
+    <AboutCompany/>
   </div>
 </template>
 
 <script>
 import Intro from '~/components/Intro.vue'
+import AboutCompany from '~/components/AboutCompany.vue'
 
 export default {
   name: 'Page',
 
   components: {
-    Intro
+    Intro,
+    AboutCompany
   }
 }
 </script>
