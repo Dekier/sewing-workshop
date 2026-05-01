91c33cbf2e047dcbea5c8ce20fa85c0685706614
Marcin Dekier <marcin.dekier97@gmail.com>
2026-05-01 21:04:52 +0200

    add logo dezal

---
+++ sewing-workshop/components/Cookies.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Cookies__main-container">
+    Ta strona używa ciasteczek (cookies) m.in. do statystyk, dzięki którym nasz
+    serwis może działać lepiej.
+    <div @click="hideCookies()" class="Cookies__btn">
+      <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+        <path
+          fill="#fff"
+          d="M28.941 31.786L.613 60.114a2.014 2.014 0 1 0 2.848 2.849l28.541-28.541 28.541 28.541c.394.394.909.59 1.424.59a2.014 2.014 0 0 0 1.424-3.439L35.064 31.786 63.41 3.438A2.014 2.014 0 1 0 60.562.589L32.003 29.15 3.441.59A2.015 2.015 0 0 0 .593 3.439l28.348 28.347z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cookies",
+  methods: {
+    hideCookies() {
+      this.$emit("hideCookies");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "Cookies";
+</style>
--- sewing-workshop/layouts/default.vue
+++ sewing-workshop/layouts/default.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    <nuxt/>
+    <nuxt />
+    <Cookies v-if="visibleCookies" @hideCookies="hideCookies" />
   </div>
 </template>
+<script>
+import Cookies from "~/components/Cookies.vue";
+export default {
+  components: {
+    Cookies
+  },
+
+  mounted() {
+    if (Boolean(localStorage.cookies)) {
+      this.visibleCookies = !Boolean(localStorage.cookies);
+    }
+  },
+
+  data() {
+    return {
+      visibleCookies: true
+    };
+  },
+  methods: {
+    hideCookies() {
+      this.visibleCookies = false;
+      localStorage.cookies = false;
+    }
+  }
+};
+</script>
+
 
 <style>
-html, body {
+html,
+body {
   margin: 0;
   padding: 0;
 }
