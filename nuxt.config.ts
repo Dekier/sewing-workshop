7848204cfbc1a08d92f6912d2533e3a03107d9bd
Marcin Dekier <marcin.dekier97@gmail.com>
2026-06-03 12:52:19 +0200

    masterts3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 export default defineNuxtConfig({
-  ssr: true,
   app: {
     head: {
       htmlAttrs: {
@@ -8,52 +7,13 @@ export default defineNuxtConfig({
     },
   },
 
-  experimental: {
-    payloadExtraction: true,
-  },
-
   routeRules: {
-    "/__og-image__/**": { cache: false },
-    // Twoje główne reguły renderowania
-    "/**": { swr: 3600 },
-
-    // Zmuszenie przeglądarki do cachowania obrazków, css, js i czcionek przez rok
-    "/_nuxt/**": {
-      headers: { "cache-control": "public, max-age=31536000, immutable" },
-    },
-    "/images/**": { headers: { "cache-control": "public, max-age=31536000" } },
-    "/*.png": { headers: { "cache-control": "public, max-age=31536000" } },
-    "/*.webp": { headers: { "cache-control": "public, max-age=31536000" } },
-    "/*.jpg": { headers: { "cache-control": "public, max-age=31536000" } },
-    "/**/*.woff2": {
-      headers: { "cache-control": "public, max-age=31536000, immutable" },
-    },
-    "/**/*.woff": {
-      headers: { "cache-control": "public, max-age=31536000, immutable" },
-    },
     "/opinia": {
       redirect: {
         to: "https://g.page/r/CRlZPkO0DZHUEBM/review",
         statusCode: 301,
       },
     },
-  },
-
-  nitro: {
-    serverAssets: [
-      {
-        baseName: "articles", // To będzie nazwa, pod którą odwołasz się w kodzie
-        dir: "app/assets/content/articles", // Ścieżka relatywna do głównego katalogu projektu
-      },
-    ],
-    prerender: {
-      crawlLinks: true,
-      failOnError: false,
-      routes: [
-        // '/',
-      ],
-    },
-    compressPublicAssets: { gzip: true, brotli: true },
   },
 
   modules: ["@nuxt/scripts", "@nuxt/fonts"],
